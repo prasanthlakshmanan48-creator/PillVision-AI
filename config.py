f4ccf36cbba82d612dc429f9eb1d5995a29b1775
prasanthlakshmanan48-creator <prasanthlakshmanan48@gmail.com>
2026-07-31 23:16:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,11 @@
+import streamlit as st
 from google import genai
 
-# ------------------------------------
-# Paste Your Gemini API Key
-# ------------------------------------
+MODEL_NAME = "gemini-3.5-flash"
 
-GEMINI_API_KEY="AQ.Ab8RN6JE5z4DbCvTB2wRJWzhd7FYjDOEAC07y6f0F58Yy3TtCQ"
-
-# ------------------------------------
-
-MODEL_NAME="gemini-3.5-flash"
-
-client=genai.Client(
-    api_key=GEMINI_API_KEY
+client = genai.Client(
+    api_key=st.secrets["AQ.Ab8RN6JE5z4DbCvTB2wRJWzhd7FYjDOEAC07y6f0F58Yy3TtCQ"]
 )
 
-APP_NAME="PillVision AI"
-
-VERSION="1.0"
-
-DEVELOPER="Your Name"
+APP_NAME = "PillVision AI"
+VERSION = "1.0"
