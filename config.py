b737063a4ea4975ee748f59ae64f07f81290d6b9
prasanthlakshmanan48-creator <prasanthlakshmanan48@gmail.com>
2026-07-31 23:32:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from google import genai
 MODEL_NAME = "gemini-3.5-flash"
 
 client = genai.Client(
-    api_key=st.secrets["AQ.Ab8RN6JE5z4DbCvTB2wRJWzhd7FYjDOEAC07y6f0F58Yy3TtCQ"]
+    api_key=st.secrets["GEMINI_API_KEY"]
 )
