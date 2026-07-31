56f41af806a57fc21e7cbae5fa651eff53a4a60c
prasanthlakshmanan48-creator <prasanthlakshmanan48@gmail.com>
2026-07-31 23:26:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,3 @@ MODEL_NAME = "gemini-3.5-flash"
 client = genai.Client(
     api_key=st.secrets["AQ.Ab8RN6JE5z4DbCvTB2wRJWzhd7FYjDOEAC07y6f0F58Yy3TtCQ"]
 )
-
-APP_NAME = "PillVision AI"
-VERSION = "1.0"
