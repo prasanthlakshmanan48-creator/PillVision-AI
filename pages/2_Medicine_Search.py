c6704201d658a0373318e6168766639c3e31f261
prasanthlakshmanan48-creator <prasanthlakshmanan48@gmail.com>
2026-07-31 22:19:21 +0530

    Add basic medicine search interface with Streamlit

--- pages/2_Medicine_Search.py
+++ pages/2_Medicine_Search.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.title("📖 Medicine Search")
+
+medicine = st.text_input("Enter Medicine Name")
+
+if st.button("Search"):
+
+    st.write("Medicine:", medicine)
+
+    st.info("AI search will be added in Version 2.")
